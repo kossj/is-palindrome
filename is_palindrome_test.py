7856d81a6c6827ff5dfcf3c2a212d067a60175d8
Jack K <52486731+sklnetAFK@users.noreply.github.com>
2023-05-24 15:10:48 +0000

    Added gitignore

--- is_palindrome_test.py
+++ is_palindrome_test.py
@@ -3,8 +3,8 @@ import is_palindrome
 solution = is_palindrome.Solution()
 
 # These are the testcases your code will be tested against.
+# You can run them as your code will be graded using the command pytest.
 
 def test():
     assert solution.isPalindrome("racecar") is True
     assert solution.isPalindrome("not_a_palindrome") is False
-
