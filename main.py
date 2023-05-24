a3a2dd95fe7db38ceafa88980a2ce8d01e162bd7
Jack K <52486731+sklnetAFK@users.noreply.github.com>
2023-05-24 09:13:09 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+class Solution:
+  # Don't worry about what self is for this assignment
+  # The str and bool are type annotations, and demarcate what parameters your function takes and returns.
+  def isPalindrome(self, s: str) -> bool:
+    # Ex. return True
