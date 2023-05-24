d513a4226ee5862235c640fb8b5e9f19afe9c759
Jack K <52486731+sklnetAFK@users.noreply.github.com>
2023-05-24 15:19:09 +0000

    pytest may be installed by default

--- is_palindrome_test.py
+++ is_palindrome_test.py
@@ -5,9 +5,6 @@ solution = is_palindrome.Solution()
 # These are the testcases your code will be tested against.
 # You can run them as your code will be graded using the command pytest.
 
-# To do this, run python -m pip install -r requirements.txt to install the required
-# pytest package. Then, simply type pytest into your terminal.
-
 def test():
     assert solution.isPalindrome("racecar") is True
     assert solution.isPalindrome("not_a_palindrome") is False
